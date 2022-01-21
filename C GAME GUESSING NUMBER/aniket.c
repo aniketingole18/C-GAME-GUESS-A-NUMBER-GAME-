ec9f5b3720938117595a5420116b12f48394a1fd
Aniket Ingole <94308631+aniketingole18@users.noreply.github.com>
2022-01-21 23:59:26 +0530

    Add files via upload

---
+++ C GAME GUESSING NUMBER/aniket.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+int main(){
+    int number, guess,nguess=1;
+    srand(time(0));
+    number=rand()%100+1;
+    do
+    {
+        printf("Guess the number between 1 to 100\n");
+        scanf("%d",&guess);
+        if (guess>number)
+        {
+          printf("Lower number please!\n");
+        }
+        else if (guess<number)
+        {
+            printf("Higher number please!\n");
+        }
+        else{
+            printf("You Guessed in %d attempts!!  !!BETTER LUCK NEXT TIME!!",nguess);
+        } 
+        nguess++;
+    } while (guess!=number);
+    return 0;
+    
+}
